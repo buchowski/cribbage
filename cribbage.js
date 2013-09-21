1876f0bde52003558873683daf794b40cb4852db
buchowski <bucholz.adam@gmail.com>
2013-09-20 18:36:19 -0700

    allow player to lay cards. add terminal colors

--- cribbage.js
+++ cribbage.js
@@ -2,6 +2,7 @@ root._ = require('./underscore.js');
 
 var VALS = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
 var SUITS = ['H', 'C', 'S', 'D'];
+var COLORS = {'green': '\033[32m', 'cyan': '\033[36m', 'black': '\033[0m'}
 
 function Card(suit, val) {
 	this.suit = suit;
@@ -38,7 +39,31 @@ function Hand() {
 Hand.prototype.push = function (card) {
 	this.cards.push(card);
 }
+Hand.prototype.show = function () {
+	var card_str = '';
+	_.each(this.cards, function (card) {
+		card_str += card.val + card.suit + ', ';
+	})
+	return card_str;
+}
 
+function Pile() {
+	this.cards = [];
+}
+Pile.prototype = Object.create(Hand.prototype);
+Pile.prototype.score = function () {
+	var total_score = 0;
+	_.each(this.cards, function (card) {
+		if ( card.val == 'A' ) { 
+			total_score += 1;
+		} else if ( isNaN(Number(card.val)) ) {
+			total_score += 10;
+		} else {
+			total_score += Number(card.val);
+		}
+	})
+	return total_score;
+}
 
 function Player(name) {
 	this.name = name;
@@ -52,13 +77,14 @@ Player.prototype.discard = function (card) {
 function Game(player1, player2) {
 	
 	this.deck = new Deck();
+	this.pile = new Pile();
 	this.current_player = player1;
 	this.dealer = player1;
 	this.player1 = player1;
 	this.player2 = player2;
 	this.discard_count = 0;
 }
-Game.prototype.prompt = function () {
+Game.prototype.discard = function () {
 	var that = this;
 	var readline = require('readline');
 	var rl = readline.createInterface({
@@ -68,30 +94,54 @@ Game.prototype.prompt = function () {
 	
 	if ( this.discard_count >= 4 ) { 
 		rl.close(); 
-		this.print_state();
+		this.play();
+		this.discard_count = 0;
 		return; 
 	} 
-
-	rl.question(this.current_player.name + ', what card would you like to discard?', function (answer) {
+	console.log(this.current_player.name + "'s hand: " + COLORS['green'] + this.current_player.hand.show() + COLORS['black']);
+	rl.question(this.current_player.name + ', what card would you like to discard?', function (card) {
 		rl.close();
-		that.dealer.crib.push(that.current_player.discard(answer));
+		that.dealer.crib.push(that.current_player.discard(card));
 		that.discard_count++;
-		that.current_player = ( that.current_player == that.player1 ) ? that.player2: that.player1;
-		that.prompt();
+		that.switch_player();
+		that.discard();
+	})
+}
+Game.prototype.switch_player = function () {
+	this.current_player = ( this.current_player == this.player1 ) ? this.player2: this.player1;
+}
+Game.prototype.play = function () {
+	var that = this;
+	var readline = require('readline');
+	var rl = readline.createInterface({
+		input: process.stdin,
+		output: process.stdout
+	});
+
+	this.switch_player();
+
+	if ( this.player1.hand.cards.length == 0 && this.player2.hand.cards.length == 0 ) {
+		rl.close();
+		this.print_state();
+		return;
+	}
+
+	console.log(this.current_player.name + "'s hand: " + COLORS['green'] + this.current_player.hand.show() + COLORS['black']);
+
+	rl.question(this.current_player.name + ', add a card to the pile.', function (card) {
+		rl.close();
+		that.pile.cards.push(that.current_player.discard(card));
+		console.log(COLORS['cyan'] + 'the pile score is: ' + that.pile.score() + COLORS['black']);
+		that.play();
 	})
 }
 
 Game.prototype.print_state = function () {
 	_.each([this.player1, this.player2], function (player) {
-		console.log(player.name + ' hand: ');
-		_.each(player.hand.cards, function (card) {
-			console.log(card);
-		})
-		console.log(player.name + ' crib: ');
-		_.each(player.crib.cards, function (card) {
-			console.log(card);
-		})
+		console.log(player.name + ' hand: ' + player.hand.show());
+		console.log(player.name + ' crib: ' + player.crib.show());
 	})
+	console.log('pile: ' + this.pile.show());
 }
 
 var jeremy = new Player('jeremy');
@@ -101,7 +151,7 @@ var game = new Game(jeremy, nathan); //The first player you pass is the dealer
 game.deck.cut();
 game.deck.shuffle();
 game.deck.deal(jeremy, nathan);
-game.prompt();
+game.discard();
 
 
 
