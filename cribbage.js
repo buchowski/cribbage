417020e5cc9d73282d60f5afc11627610674c837
buchowski <bucholz.adam@gmail.com>
2013-10-06 18:22:57 -0700

    disallow invalid pushes

--- cribbage.js
+++ cribbage.js
@@ -5,6 +5,7 @@
 	var SUITS = CRIBBAGE.SUITS = ['H', 'C', 'S', 'D'];
 
 	var Card = CRIBBAGE.Card = function (suit, val) {
+		this.holder = null;
 		this.suit = suit;
 		this.val = val;
 	}
@@ -13,7 +14,8 @@
 		return ( isNaN(num) ) ? ((this.val == 'A') ? 1 : 10 ): num;
 	};
 
-	var Deck = CRIBBAGE.Deck = function () {
+	var Deck = CRIBBAGE.Deck = function (game) {
+		this.game = game;
 		this.cut_card = null;
 		this.cards = [];
 		for (var i = 0; i < SUITS.length; i++ ) {
@@ -21,7 +23,7 @@
 				this.cards.push(new Card(SUITS[i], VALS[j]));
 			}
 		}
-	}
+	};
 	Deck.prototype.shuffle = function () {
 		this.cards = _.shuffle(this.cards);
 	};
@@ -29,46 +31,89 @@
 		var index = Math.floor(Math.random() * this.cards.length);
 		this.cut_card = this.cards.splice(index, 1)[0];
 	};
+	Deck.prototype.push = function (card) {
+		var hand = this.game.round.current_player.hand;
+		var round = this.game.round;
+
+		this.cards.push(hand.cards.splice(hand.get_card_index(card[0], card[1]), 1)[0]);
+		round.discard_count++;
+		round.switch_player();
+	};
 
 	var Hand = CRIBBAGE.Hand = function () {
 		this.cards = [];
 		this.score = 0;
-	}
+	};
 	Hand.prototype.push = function (card) {
 		this.cards.push(card);
-		this.update_score(card);
+	};
+	
+	Hand.prototype.get_card_index = function (val, suit) {
+		for (var i = 0; i < this.cards.length; i++) {
+			var card = this.cards[i];
+			if (card.val == val && card.suit == suit) return i;
+		}
 	}
-	Hand.prototype.is_valid_push = function (card) {
+
+	var Pile = CRIBBAGE.Pile = function (game) {
+		this.game = game;
+		this.hand = new Hand();
+		this.score = 0;
+		this.name = 'Pile';
+	}
+	Pile.prototype.push = function (card) {
+		var round = this.game.round;
+		var hand = round.current_player.hand;
+		var card_index = hand.get_card_index(card[0], card[1]);
+
+		if (this.is_valid_push(hand.cards[card_index])) {
+			card = hand.cards.splice(card_index, 1)[0];
+			this.hand.cards.push(card);
+			this.update_score(card);
+			round.discard_count++;
+			round.switch_player();
+		} else {
+			alert('invalid push');
+		}
+	}
+	Pile.prototype.is_valid_push = function (card) {
 		return ( this.score + card.int_val() <= 31 );
 	}
-	Hand.prototype.update_score = function (card) {
+	Pile.prototype.update_score = function (card) {
 		this.score += card.int_val();
-	}
-	Hand.prototype.has_playable_card = function (game) {
-		return _.some(this.cards, function (card) {
-			return game.pile.is_valid_push(card);
-		})
-	}
-	Hand.prototype.reset_score = function () {
-		this.score = 0;
-	}
-	Hand.prototype.is_score = function (num) {
-		return this.score == num;
-	}
-	Hand.prototype.is_empty = function () {
-		return this.cards.length == 0;
-	}
+		// if ( this.score == 15 || this.score == 31 ) {
+		// 	card.holder.score += 2;
+		// } else if ( this.score == 31 ) {
+		// 	card.holder.score += 2;
+		// 	this.score = 0;
+		// }
+	};
+
+
+	// Hand.prototype.has_playable_card = function (game) {
+	// 	return _.some(this.cards, function (card) {
+	// 		return game.pile.is_valid_push(card);
+	// 	})
+	// }
+	// Hand.prototype.reset_score = function () {
+	// 	this.score = 0;
+	// }
+	// Hand.prototype.is_score = function (num) {
+	// 	return this.score == num;
+	// }
+	// Hand.prototype.is_empty = function () {
+	// 	return this.cards.length == 0;
+	// }
 	var Round = CRIBBAGE.Round = function (game) {
-		// should Round store the crib?
 		this.game = game;
 		this.current_player = game.players[0];
 		this.dealer = game.players[1];
 		this.discard_count = 0;
-	}
+	};
 	Round.prototype.switch_player = function () {
 		var players = this.game.players;
 		this.current_player = players[( players.indexOf(this.current_player ) + 1) % 2];
-	}
+	};
 
 	var Player = CRIBBAGE.Player = function (name, game) {
 		this.name = name;
@@ -76,68 +121,70 @@
 		this.hand = new Hand();
 		this.crib = new Hand();
 		this.score = 0;
-	}
+	};
 	Player.prototype.discard = function (val, suit, group, render) {
 		var round = this.game.round;
-		var cards = this.hand.cards;
-		group.cards.push(cards.splice(cards.indexOf({suit: suit, val: val}), 1)[0]);
+		var hand = this.hand;
+
+		group.push(hand.cards.splice(hand.get_card_index(val, suit), 1)[0])
 		round.discard_count++;
 		round.switch_player();
 		render();
 	};
-	Player.prototype.card = function (card) {
-		return this.hand.cards.slice(card - 1)[0];
-	}
+	// Player.prototype.card = function (card) {
+	// 	return this.hand.cards.slice(card - 1)[0];
+	// }
 	var Game = CRIBBAGE.Game = function (player1, player2, view) {
 		this.view = view;
-		this.deck = new CRIBBAGE.Deck();
-		this.pile = new CRIBBAGE.Hand();
+		this.deck = new CRIBBAGE.Deck(this);
+		this.pile = new CRIBBAGE.Pile(this);
 		this.players = [new CRIBBAGE.Player(player1, this), new CRIBBAGE.Player(player2, this)];
 		this.round = new Round(this);
-	}
+	};
 	Game.prototype.deal = function () {
 		var game = this;
 		_.times(12, function (n) {
 			var card = game.deck.cards.pop();
+			card.holder = game.players[ n % 2 ];
 			game.players[ n % 2 ].hand.push(card);
 		})
-	}
-	Game.prototype.switch_player = function () {
-		this.current_player = ( this.current_player == this.player1 ) ? this.player2: this.player1;
-	}
-	Game.prototype.register_cards = function () {
-		_.each([this.player1, this.player2], function (player) {
-			_.each(player.hand.cards, function (card) {
-				card.player = player.name;
-			})
-		})
-	}
-	Game.prototype.increment_discard_count = function () {
-		this.discard_count++;
-	}
-	Game.prototype.reset_discard_count = function () {
-		this.discard_count = 0;
-	}
-	Game.prototype.cards_discarded = function () {
-		return this.discard_count >= 4;
-	}
-	Game.prototype.all_hands_played = function () {
-		return (this.player1.hand.cards.length == 0 && this.player2.hand.cards.length == 0);
-	}
-	Game.prototype.add_points = function (num) {
-		this.current_player.score += num;
-	}
-	Game.prototype.award_points = function () {
-		if ( this.pile.is_score(15) || this.pile.is_score(31) ) { 
-			this.add_points(2); 
-		}
-		if ( this.pile.is_score(31) ) {
-			this.pile.reset_score(); 
-		}
-	}
-	Game.prototype.is_round_over = function () {
-		return ( this.player1.hand.cards.length == 0 && this.player2.hand.cards.length == 0 );
-	}
+	};
+	// Game.prototype.switch_player = function () {
+	// 	this.current_player = ( this.current_player == this.player1 ) ? this.player2: this.player1;
+	// }
+	// Game.prototype.register_cards = function () {
+	// 	_.each([this.player1, this.player2], function (player) {
+	// 		_.each(player.hand.cards, function (card) {
+	// 			card.player = player.name;
+	// 		})
+	// 	})
+	// }
+	// Game.prototype.increment_discard_count = function () {
+	// 	this.discard_count++;
+	// }
+	// Game.prototype.reset_discard_count = function () {
+	// 	this.discard_count = 0;
+	// }
+	// Game.prototype.cards_discarded = function () {
+	// 	return this.discard_count >= 4;
+	// }
+	// Game.prototype.all_hands_played = function () {
+	// 	return (this.player1.hand.cards.length == 0 && this.player2.hand.cards.length == 0);
+	// }
+	// Game.prototype.add_points = function (num) {
+	// 	this.current_player.score += num;
+	// }
+	// Game.prototype.award_points = function () {
+	// 	if ( this.pile.is_score(15) || this.pile.is_score(31) ) { 
+	// 		this.add_points(2); 
+	// 	}
+	// 	if ( this.pile.is_score(31) ) {
+	// 		this.pile.reset_score(); 
+	// 	}
+	// }
+	// Game.prototype.is_round_over = function () {
+	// 	return ( this.player1.hand.cards.length == 0 && this.player2.hand.cards.length == 0 );
+	// }
 })(this);
 
 
--- cribbage_web_ui.js
+++ cribbage_web_ui.js
@@ -3,44 +3,39 @@
 
 	var View = CRIBBAGE.View = function (player1, player2) {
 		this.game = new CRIBBAGE.Game(player1, player2, this);
-	}
+	};
 	View.empty_view = function () {
 		$('.view').empty();
-	}
+	};
 	View.card_template = function (card) {
 		return "<div class='card " + card.suit + "' id='" + card.val + card.suit + "'>" + card.val + card.suit + "</div>";
-	}
+	};
 	View.player_template = function (player) {
-		return "<div class='name' id='" + player.name + "'><h1>" + player.name + "'s Cards:" + "</h1></div>";
-	}
-	View.prompt_template = function (player, msg) {
-		return "<h1 class='name'>" + player.name + msg + "</h1>";
-	}
+		return "<h1 class='name'>" + player.name + "'s Score: " + player.score + "</h1>" +
+				"<div class='name' id='" + player.name + "'><h1>" + player.name + "'s Cards:" + "</h1></div>";
+	};
+	View.prompt_template = function (name, msg) {
+		return "<h1 class='name'>" + name + msg + "</h1>";
+	};
 	View.get_val_suit = function($el) {
 		var val = $el.attr('id')[0];
 		var val = ( val == '1') ? '10' : val;
 		var suit = ( val == '10') ? $el.attr('id')[2] : $el.attr('id')[1];
 		return [val, suit];
-	}
+	};
 	View.prototype.render = function () {
 		var game = this.game;
 		$("#game").empty();
 		$('#game').append("<h1>" + game.round.dealer.name + " is the dealer</h1>");
 		$("#game").append("<h1>Cut Card:</h1>" + View.card_template(game.deck.cut_card));
-		$("#game").append("<h1 class='name'>Pile:</h1>");
-		
-		_.each(game.pile.cards, function (card) {
-			$('#game').append(View.card_template(card));
-		})
 
-		_.each(game.players, function (player) {
-			$('#game').append(View.prompt_template(player, "'s Score: " + player.score))
+		_.each([game.pile, game.players[0], game.players[1]], function (player) {
 			$('#game').append(View.player_template(player));
 			_.each(player.hand.cards, function (card) {
 				$('#' + player.name).append(View.card_template(card));
 			})
 		})
-		$("#game").append(View.prompt_template(game.round.current_player, ", please discard a card."));
+		$("#game").append(View.prompt_template(game.round.current_player.name, ", please discard a card."));
 		this.bind_clicks();
 	}
 	View.prototype.start = function () {
@@ -51,14 +46,33 @@
 		game.deal();
 
 		this.render();
-	}
+	};
 	View.prototype.bind_clicks = function () {
 		var game = this.game;
 		var view = this;
-		var group = (game.round.discard_count < 4) ? game.deck : game.pile ;
-		$("#" + game.round.current_player.name).on('click', '.card', function () {
-			var card = View.get_val_suit($(this));
-			game.round.current_player.discard(card[0], card[1], group, view.render.bind(view));
-		});
-	}
+		var count = game.round.discard_count;
+		// var group = (game.round.discard_count < 4) ? game.round.dealer.crib : game.pile;
+		var name = game.round.current_player.name;
+
+		if (count < 4) {
+			$("#" + name).on('click', '.card', function () {
+				var card = View.get_val_suit($(this));
+				game.deck.push(card);
+				view.render();
+			})
+		} else if (count < 12) {
+			$("#" + name).on('click', '.card', function () {
+				var card = View.get_val_suit($(this));
+				game.pile.push(card);
+				view.render();
+			})
+		} else {
+			alert("el fin del partido");
+		}
+
+		// $("#" + game.round.current_player.name).on('click', '.card', function () {
+		// 	var card = View.get_val_suit($(this));
+		// 	game.round.current_player.discard(card[0], card[1], group, view.render.bind(view));
+		// });
+	};
 })(this);
