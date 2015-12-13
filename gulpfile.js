ab881d7b2b64fe080945aad240bf6f9fb54f3af0
buchowski <bucholz.adam@gmail.com>
2015-12-13 12:54:08 -0800

    setup up gulp watch

--- gulpfile.js
+++ gulpfile.js
@@ -18,8 +18,18 @@ gulp.task('copyjs', function () {
 });
 
 gulp.task('copycss', function () {
-	return gulp.src(['src/css/**/*.css'])
+	return gulp.src(['src/css/*.css'])
         .pipe(gulp.dest('web/css'));
 });
 
-gulp.task('default', ['babelify', 'copyjs', 'copycss']);
+gulp.task('copyvendorcss', function () {
+	return gulp.src(['src/css/vendor/*.css'])
+        .pipe(gulp.dest('web/css/vendor'));
+});
+
+gulp.task('watch', function () {
+	gulp.watch(['src/js/*.js'], ['babelify']);
+	gulp.watch(['src/css/*.css'], ['copycss']);
+});
+
+gulp.task('default', ['babelify', 'copyjs', 'copycss', 'copyvendorcss', 'watch']);
